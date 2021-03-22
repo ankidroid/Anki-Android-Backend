3e7df6c7058a5ce58be71869dd3f8fcda8967924
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-03-22 17:14:22 +0000

    Fix macOS CI: NDK & Python
    Errors
    
    Python -
        distutils.errors.CompileError: command 'x86_64-linux-gnu-gcc' failed with exit status 1
    
    NDK -
    
    mkdir: /Users/runner/Library/Android/sdk/ndk/22.0.7026061/: File exists
    mv: rename 22.0.7026061/android-ndk-r22/build to /Users/runner/Library/Android/sdk/ndk/22.0.7026061/build: Directory not empty

--- .github/scripts/install_ndk_macos.sh
+++ .github/scripts/install_ndk_macos.sh
@@ -1,8 +1,14 @@
 : ${2?"Usage: $0 NDK Short Version (r22), NDK Long Version (22.0.7026061) "}
 # We don't use sdkmanager due to https://stackoverflow.com/questions/46402772/failed-to-install-android-sdk-java-lang-noclassdeffounderror-javax-xml-bind-a
-echo 'Downloading NDK'
-wget --no-verbose -O android-ndk.zip "https://dl.google.com/android/repository/android-ndk-$1-darwin-x86_64.zip"
-echo 'Unzipping NDK'
-unzip -q -d $2 android-ndk.zip
-mkdir $ANDROID_SDK_ROOT/ndk/$2/
-mv $2/*/* $ANDROID_SDK_ROOT/ndk/$2
+
+if [ ! -d "$ANDROID_SDK_ROOT/ndk/$2/" ]
+then
+  echo 'Downloading NDK'
+  wget --no-verbose -O android-ndk.zip "https://dl.google.com/android/repository/android-ndk-$1-darwin-x86_64.zip"
+  echo 'Unzipping NDK'
+  unzip -q -d $2 android-ndk.zip
+  mkdir $ANDROID_SDK_ROOT/ndk/$2/
+  mv $2/*/* $ANDROID_SDK_ROOT/ndk/$2
+else
+  echo 'Skipping NDK download - already installed'
+fi
