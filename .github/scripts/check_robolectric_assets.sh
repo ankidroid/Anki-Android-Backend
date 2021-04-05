5f16c4efbabc7ed2e2dbc3621bb3976a8293a231
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-04-05 20:03:50 +0100

    Add CI warning if GCC linked dlls found
    A user using robolectric for windows will need to have GCC installed
    
    This isn't ideal, but I wasn't able to find the correct linker
    documentation to disable this behavior
    
    Related: #46

--- .github/scripts/check_robolectric_assets.sh
+++ .github/scripts/check_robolectric_assets.sh
@@ -19,3 +19,13 @@ then
 	echo "Error: Expected 1 .dll file" 
 	exit 1
 fi
+
+# list the libraries
+objdump --private-headers rsdroid.dll  | grep "DLL Name:"
+
+objdump --private-headers rsdroid.dll  | grep "DLL Name:" | grep "libgcc"
+hasGcc=$?
+if [ "$hasGcc" -eq 0  ]
+then
+	echo "[WARN]: libgcc module found. Users will need to install GCC on Windows (#46)"
+fi
