257d68de6614aa197bda3b878277617be4b4ed21
David Allison <62114487+david-allison-1@users.noreply.github.com>
2021-07-07 19:10:24 +0100

    extract functionality to V11 SQLite Opener
    In prep for a V16 version

--- rsdroid/src/test/java/net/ankiweb/CollectionCreationTest.java
+++ rsdroid/src/test/java/net/ankiweb/CollectionCreationTest.java
@@ -11,7 +11,7 @@ import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import net.ankiweb.rsdroid.BackendFactory;
-import net.ankiweb.rsdroid.database.RustSupportSQLiteOpenHelper;
+import net.ankiweb.rsdroid.database.RustV11SupportSQLiteOpenHelper;
 import net.ankiweb.rsdroid.testing.RustBackendLoader;
 
 import org.junit.Before;
@@ -44,7 +44,7 @@ public class CollectionCreationTest {
 
         Configuration config = getConfiguration(path);
 
-        SupportSQLiteDatabase database = new RustSupportSQLiteOpenHelper(config, backendV1).getWritableDatabase();
+        SupportSQLiteDatabase database = new RustV11SupportSQLiteOpenHelper(config, backendV1).getWritableDatabase();
 
         database.beginTransaction();
         try {
