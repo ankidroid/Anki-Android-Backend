54b16a2469f4a744b1431478c9bf455d47269fca
Mike Hardy <github@mikehardy.net>
2022-04-21 12:06:45 -0500

    fix: install latest command line tools, use them for NDK

--- .github/scripts/install_ndk.sh
+++ .github/scripts/install_ndk.sh
@@ -1,4 +1,7 @@
 : ${1?"Usage: $0 NDK Version (ex 21.3.6528147)"}
 
 echo "installing NDK $1"
-echo "y" | $(sudo $ANDROID_SDK_ROOT/tools/bin/sdkmanager --install "ndk;$1" --sdk_root=${ANDROID_SDK_ROOT}) | grep -v = || true
+echo $PATH
+sdkmanager --list_installed|grep ndk
+sdkmanager --install "ndk;$1" --sdk_root=${ANDROID_SDK_ROOT} | grep -v =
+sdkmanager --list_installed|grep ndk
