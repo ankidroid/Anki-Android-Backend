dd19004e538f5c4603b02b90b23e3473fdf5b2b5
Damien Elmes <gpg@ankiweb.net>
2022-06-22 15:27:41 +1000

    Log stdout/stderr to ease println() debugging
    Android sends it to /dev/null by default

--- rslib-bridge/src/logging.rs
+++ rslib-bridge/src/logging.rs
@@ -1,10 +1,16 @@
 //! A simple adaptor that takes log messages from the backend and sends them to
 //! the Android logs.
+//! It also captures stdout/stderr output, and feeds it to logcat, to make it
+//! easier to debug issues with dbg!()/println!()
 
 use android_logger::Config;
 use log::Level;
 use slog::*;
+use std::io::{BufRead, BufReader};
+use std::time::Duration;
 use std::{fmt, result};
+
+use gag::BufferRedirect;
 
 pub struct AndroidSerializer;
 
@@ -38,7 +44,34 @@ impl Drain for AndroidDrain {
     }
 }
 
+fn redirect_io() -> Result<()> {
+    monitor_io_handle(BufferRedirect::stdout()?);
+    monitor_io_handle(BufferRedirect::stderr()?);
+    Ok(())
+}
+
+fn monitor_io_handle(handle: BufferRedirect) {
+    let mut handle = BufReader::new(handle);
+
+    std::thread::spawn(move || {
+        let mut buf = String::new();
+        loop {
+            buf.truncate(0);
+            match handle.read_line(&mut buf) {
+                Ok(0) => {
+                    // currently EOF
+                    std::thread::sleep(Duration::from_secs(1));
+                }
+                Ok(_) => log::debug!("{}", buf),
+                Err(err) => log::debug!("stdio err: {}", err),
+            }
+        }
+    });
+}
+
 pub(crate) fn setup_logging() -> Logger {
+    // failure is expected after the first backend invocation
+    let _ = redirect_io();
     android_logger::init_once(Config::default().with_min_level(Level::Debug));
     Logger::root(AndroidDrain {}.fuse(), slog_o!())
 }
