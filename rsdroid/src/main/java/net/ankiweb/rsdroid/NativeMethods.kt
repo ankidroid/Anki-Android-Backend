01ebf37ed089c05438525e45ff5843f71e501c0d
Damien Elmes <gpg@ankiweb.net>
2022-06-26 13:50:46 +1000

    Update OVERVIEW.md
    I tried using the site to import the docs/source files, but it told me
    it wasn't a valid file type, so I've just removed the diagrams for now.

--- rsdroid/src/main/java/net/ankiweb/rsdroid/NativeMethods.kt
+++ rsdroid/src/main/java/net/ankiweb/rsdroid/NativeMethods.kt
@@ -32,9 +32,9 @@ object NativeMethods {
     }
 
     @CheckResult
-    external fun runMethodRaw(backendPointer: Long, service: Int, method: Int, args: ByteArray?): Array<ByteArray?>?
+    external fun runMethodRaw(backendPointer: Long, service: Int, method: Int, args: ByteArray): Array<ByteArray?>?
 
     @CheckResult
-    external fun openBackend(data: ByteArray?): Array<ByteArray?>?
+    external fun openBackend(data: ByteArray): Array<ByteArray?>?
     external fun closeBackend(backendPointer: Long)
 }
