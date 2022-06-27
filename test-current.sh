d5c26f4671d42f8c1d23e17a0beb623cc40ea255
Damien Elmes <gpg@ankiweb.net>
2022-06-27 12:30:52 +1000

    Use connectedAndroidTest
    https://github.com/ankidroid/Anki-Android-Backend/issues/209#issuecomment-1166690348

--- test-current.sh
+++ test-current.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-NO_CROSS=true CURRENT_ONLY=true ./gradlew rsdroid:test rsdroid-instrumented:assembleDebugAndroidTest
-adb shell am instrument -w -e debug false net.ankiweb.rsdroid.instrumented.test/androidx.test.runner.AndroidJUnitRunner
+NO_CROSS=true CURRENT_ONLY=true ./gradlew rsdroid:test rsdroid-instrumented:connectedAndroidTest
+
