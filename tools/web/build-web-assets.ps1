ba7487344a09ee78b5edd164c7c914101dbd9487
Mani <12841290+krmanik@users.noreply.github.com>
2022-07-20 00:12:42 +0800

    Build anki web assets from rslib-bridge/anki submodule (#217)

---
+++ tools/web/build-web-assets.ps1
@@ -0,0 +1,21 @@
+$SRC_DIR = (Get-Item .).FullName
+$ROOT_DIR = (Get-Item D:/a/).FullName
+$ANKI_SRC = (Get-Item $SRC_DIR/rslib-bridge/anki).FullName
+$TEMP_DIR = (Get-Item $ENV:TEMP).FullName
+
+New-Item -P $SRC_DIR/rsdroid/build/generated/anki_artifacts/web -itemType Directory
+$BUILD_DIR = (Get-Item $SRC_DIR/rsdroid/build/generated/anki_artifacts).FullName
+
+Set-Location -Path $ANKI_SRC
+
+bazel --output_user_root=$ROOT_DIR/_bzl build -c opt buildinfo.txt --symlink_prefix=$TEMP_DIR/.bazel/ --verbose_failures
+Copy-Item -Path $TEMP_DIR/.bazel/bin/buildinfo.txt -Destination $BUILD_DIR/buildinfo.txt -PassThru
+
+bazel --output_user_root=$ROOT_DIR/_bzl build ts/reviewer/reviewer_extras_bundle --symlink_prefix=$TEMP_DIR/.bazel/ --verbose_failures
+Copy-Item -Path $TEMP_DIR/.bazel/bin/ts/reviewer/reviewer_extras_bundle.js -Destination $BUILD_DIR/web -PassThru
+
+bazel --output_user_root=$ROOT_DIR/_bzl build qt/aqt/data/web/pages --symlink_prefix=$TEMP_DIR/.bazel/ --verbose_failures
+Get-Childitem $TEMP_DIR/.bazel/bin/qt/aqt/data/web/pages -Recurse -Include *.html,*.js,*.css | Copy-Item -Destination $BUILD_DIR/web -PassThru
+
+Copy-Item -Path $ANKI_SRC/cargo/licenses.json -Destination $BUILD_DIR/web/licenses-cargo.json -PassThru
+Copy-Item -Path $ANKI_SRC/ts/licenses.json -Destination $BUILD_DIR/web/licenses-ts.json -PassThru
---
+++ tools/web/build-web-assets.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# build web assets with anki submodule in rslib-bridge
+
+# check if bazel exist or not
+if ! bazel --version > /dev/null 2>&1; then
+  echo "bazel: command not found. Please install Bazelisk. Your distro may have it,"
+  echo "or you can fetch the binary from https://github.com/bazelbuild/bazelisk/releases"
+  echo "and rename it to /usr/local/bin/bazel"
+  exit 1
+fi
+
+SRC_DIR=$(pwd)
+ANKI_SRC=$SRC_DIR/rslib-bridge/anki
+BUILD_DIR=$SRC_DIR/rsdroid/build/generated/anki_artifacts
+mkdir -p $BUILD_DIR/web
+
+# build buildinfo.txt, reviewer_extras_bundle and pages files
+cd $ANKI_SRC
+
+bazel build --symlink_prefix=/tmp/.bazel/ -c opt buildinfo.txt
+cp /tmp/.bazel/bin/buildinfo.txt $BUILD_DIR/buildinfo.txt
+
+bazel build ts/reviewer/reviewer_extras_bundle --symlink_prefix=/tmp/.bazel/
+cp /tmp/.bazel/bin/ts/reviewer/reviewer_extras_bundle.js $BUILD_DIR/web
+
+bazel build qt/aqt/data/web/pages --symlink_prefix=/tmp/.bazel/
+cp -r /tmp/.bazel/bin/qt/aqt/data/web/pages/* $BUILD_DIR/web
+
+cp $ANKI_SRC/cargo/licenses.json $BUILD_DIR/web/licenses-cargo.json
+cp $ANKI_SRC/ts/licenses.json $BUILD_DIR/web/licenses-ts.json
+
+chmod -R a+w $BUILD_DIR/web
