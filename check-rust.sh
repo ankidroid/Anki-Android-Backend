97b63a168e103134c460532b2aa5f263746d67a2
Mike Hardy <github@mikehardy.net>
2023-04-01 09:13:39 -0500

    build(deps): anki upstream 2.1.61

--- check-rust.sh
+++ check-rust.sh
@@ -2,6 +2,5 @@
 
 set -e
 
-rustup component add rustfmt clippy
-cargo clippy
-cargo fmt -- --check
+rustup component add clippy && cargo clippy
+(cd anki/cargo/format && cargo fmt --check --all --manifest-path ../../../Cargo.toml)
