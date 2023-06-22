d9adb8d32ba25a502ca860eaf0372934dd95470c
Damien Elmes <gpg@ankiweb.net>
2023-06-22 20:09:48 +1000

    Fix Android library failing to run on x86 when built with latest NDK

--- rslib-bridge/build.rs
+++ rslib-bridge/build.rs
@@ -4,7 +4,10 @@
 pub mod fluent;
 pub mod proto;
 
-use std::{env, path::PathBuf};
+use std::{
+    env::{self, consts::OS},
+    path::PathBuf,
+};
 
 use anyhow::Result;
 use prost_reflect::DescriptorPool;
@@ -16,6 +19,24 @@ fn main() -> Result<()> {
     let (_, services) = anki_proto_gen::get_services(&pool);
     proto::write_kotlin_interface(&services)?;
     fluent::write_translations();
+
+    // Work around issue with recent NDKs. Based on
+    // https://github.com/bendk/application-services/commit/0443ba6f42d1964c214ff44c1f2a61689bf1dbf2
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    if target_arch == "x86_64" && target_os == "android" {
+        let android_ndk_home = env::var("ANDROID_NDK_HOME").expect("ANDROID_NDK_HOME not set");
+        let platform = if OS == "linux" {
+            "linux-x86_64"
+        } else if OS == "macos" {
+            "darwin-x86_64"
+        } else {
+            "windows-x86_64"
+        };
+        let lib_dir = format!("/toolchains/llvm/prebuilt/{platform}/lib64/clang/14.0.7/lib/linux/");
+        println!("cargo:rustc-link-search={android_ndk_home}/{lib_dir}");
+        println!("cargo:rustc-link-lib=static=clang_rt.builtins-x86_64-android");
+    }
 
     Ok(())
 }
