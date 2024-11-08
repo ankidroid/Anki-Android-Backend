4f4f4ff3284345537f3229667f5e37ab4d887f0f
David Allison <62114487+david-allison@users.noreply.github.com>
2024-11-08 19:56:50 +0000

    docs: clarify 'ANDROID_NDK_HOME' issue
    This was shown as:
    
    ```
        Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.07s
         Running `target/debug/build_rust`
    thread 'main' panicked at build_rust/src/main.rs:25:9:
    Expected ANDROID_NDK_HOME to point to a 27.x NDK. Future versions may work, but are untested.
    note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
    ```
    
    It was not immediately clear that 'Expected' was a failure
    and the error message was a little verbose

--- build_rust/src/main.rs
+++ build_rust/src/main.rs
@@ -23,7 +23,8 @@ fn main() -> Result<()> {
     }
     let ndk_path = Utf8PathBuf::from(env::var("ANDROID_NDK_HOME").unwrap_or_default());
     if !ndk_path.file_name().unwrap_or_default().starts_with("27.") {
-        panic!("Expected ANDROID_NDK_HOME to point to a 27.x NDK. Future versions may work, but are untested.");
+        // Future NDKs may work, but are untested.
+        panic!("error: ANDROID_NDK_HOME must point to a 27.x NDK.");
     }
 
     build_web_artifacts()?;
