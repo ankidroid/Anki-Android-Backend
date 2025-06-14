c94800894a6e3efb5956925d257adb2ff9085636
Mike Hardy <github@mikehardy.net>
2025-06-14 13:04:21 -0500

    style(lint): non-zero exit on clippy warnings
    previously, you could run `./check-rust.sh` for example, and it may
    give you a warning, but it would still be a successful exit code
    
    CI runs this in build-quick but without the error exit, it would
    false-negative and pass the build
    
    In this way, lint warnings could creep into the code with no notice

--- check-rust.sh
+++ check-rust.sh
@@ -2,5 +2,8 @@
 
 set -e
 
+# Non-zero exit on warnings
+export RUSTFLAGS="-Dwarnings"
+
 rustup component add clippy && cargo clippy
 (cd anki/cargo/format && cargo fmt --check --all --manifest-path ../../../Cargo.toml)
