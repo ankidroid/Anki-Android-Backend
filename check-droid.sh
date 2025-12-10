38735a44ddadd996105117e86d82a3196e55c266
Mike Hardy <github@mikehardy.net>
2025-12-10 10:13:35 -0500

    build: set NDK home in developer run scripts
    this is an ergonomic enhancement and is enabled by previous work
    to centralize the NDK version - the NDK version we use is typically
    not the system default and developers previously had to set it manually
    every time they wanted to work on Anki-Android-Backend which was annoying
    
    note that an attempt was made to do similar for Windows but it proved
    unworkable in the timebox I had due to path-slashification issues combined
    with strange windows environment variable passing rules

--- check-droid.sh
+++ check-droid.sh
@@ -6,4 +6,7 @@ test -f rsdroid/build/outputs/aar/rsdroid-release.aar || (
     echo "Run ./build.sh first"
     exit 1
 )
+
+. ./set-android-ndk-home.sh
+
 ./gradlew rsdroid:lint rsdroid-instrumented:connectedCheck
