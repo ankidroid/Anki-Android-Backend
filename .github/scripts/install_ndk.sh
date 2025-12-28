37f3457dc136f738297553609ae5565658286e42
Mike Hardy <github@mikehardy.net>
2025-12-28 14:15:35 -0500

    test(ci): use community NDK install action
    note that CodeQL runs on ubuntu which runs out of space, and our purge
    script doesn't work there, so liberate space the way we normally do
    on ubuntu
    
    also the way we should use the action is to not add it to local cache
    (which is creates a copy for no reason, in my opinion) but we should
    "link to sdk" which copies it into android home, which is where our
    workflows expect to find it

--- .github/scripts/install_ndk.sh
+++
@@ -1,8 +0,0 @@
-: ${1?"Usage: $0 NDK Version (ex 21.3.6528147)"}
-
-echo "installing NDK $1"
-echo $PATH
-sdkmanager --list_installed|grep ndk
-yes | sdkmanager --licenses
-sdkmanager --install "ndk;$1" --sdk_root=${ANDROID_SDK_ROOT} | grep -v =
-sdkmanager --list_installed|grep ndk
